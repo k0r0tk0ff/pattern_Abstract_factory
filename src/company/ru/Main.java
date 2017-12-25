af0520ebd0e8936c3e4dc3cc42d25f23aee84584
k0r0tk0ff <korotkov34reg@gmail.com>
2017-12-25 21:24:28 +0300

    add code

--- src/company/ru/Main.java
+++ src/company/ru/Main.java
@@ -1,10 +1,24 @@
 package company.ru;
 
+import company.ru.Car.Car;
+import company.ru.Tank.Tank;
+
 public class Main {
 
     public static void main(String[] arg) {
-        AbstractFactory abstractFactory = new AbstractFactory();
-        Factory factory = abstractFactory.createFactory("CarFactory");
-        factory.createCar("Audi").drive();
+        AbstractFactory abstractCarFactory = new AbstractFactory();
+        Factory carFactory = abstractCarFactory.createFactory("CarFactory");
+        Car audi = carFactory.createCar("Audi");
+        Car toyota = carFactory.createCar("Toyota");
+
+        AbstractFactory abstractTankFactory = new AbstractFactory();
+        Factory tankFactory = abstractTankFactory.createFactory("TankFactory");
+        Tank t34 = tankFactory.createTank("T34");
+        Tank pantera = tankFactory.createTank("Pantera");
+
+        audi.drive();
+        toyota.drive();
+        t34.drive();
+        pantera.drive();
     }
 }
